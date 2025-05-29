74d7c8b0cf2e41f9a3a15301a5b07328a259e6ef
h0uu0h <houjq777@gmail.com>
2025-05-29 17:21:51 +0800

    build test4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,33 +3,16 @@ import "./App.css";
 import BlinkGame from "./components/BlinkGame";
 
 function App() {
-    const BASE_URL = import.meta.env.VITE_SOCKET_URL;
     const [gameStarted, setGameStarted] = useState(false);
-    const handleToggleGame = async () => {
-        if (!gameStarted) {
-            const res = await fetch(`${BASE_URL}/start_stream`, {
-                method: "POST",
-            });
-            const result = await res.json();
-            if (result.status === "started") {
-                setGameStarted(true); // ✅ 确认后才开始
-            }
-        } else {
-            const res = await fetch(`${BASE_URL}/stop_stream`, {
-                method: "POST",
-            });
-            const result = await res.json();
-            if (result.status === "stopped") {
-                setGameStarted(false); // ✅ 确认后再卸载组件
-            }
-        }
+
+    const handleToggleGame = () => {
+        setGameStarted((prev) => !prev);
     };
 
     return (
         <div
             className="app-container"
             style={{ textAlign: "center", paddingTop: "20px" }}>
-            {/* <h1>Blink Game</h1> */}
             <button
                 onClick={handleToggleGame}
                 style={{
